d3ac212e772f4e4ccd67d7a3f0402dc8690f6d51
isihyeon <tlgus0113@gmail.com>
2024-02-13 19:41:13 +0900

    [Feat] 만다라트 객체 생성
    - issue : json_serializable 패키지 사용

---
+++ mandalart/lib/DataModel/Mandalart.dart
@@ -0,0 +1,24 @@
+class Mandalart {
+  final int _level;
+  final String _content;
+  List<Mandalart>? _specificGoals;
+
+  Mandalart(this._level, this._content);
+
+  List<Mandalart> get specificGoals => _specificGoals!;
+
+  set specificGoals(List<Mandalart> value) {
+    _specificGoals = value;
+  }
+
+  @override
+  String toString() {
+    return '{"Mandalart" : {"level": "$_level", "content": "$_content", "specificGoals": $_specificGoals}}\n';
+  }
+
+  //Todo
+  Map<String, dynamic> toJSon() {
+    // json_seriaslizable 사용
+    return ;
+  }
+}
