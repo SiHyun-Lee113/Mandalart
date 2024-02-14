6501d6fc7c12cc937af38a6527d6dde6d9038553
isihyeon <tlgus0113@gmail.com>
2024-02-14 22:10:24 +0900

    [Feat] 만다라트 생성 UI 생성 및 관련 Util 메서드 생성

---
+++ mandalart/lib/util/style/MandalartStyle.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+ShapeDecoration squareStyle() {
+  return ShapeDecoration(
+      color: Color(0xFFD9D9D9),
+      shape: RoundedRectangleBorder(
+        side: BorderSide(width: 1),
+        borderRadius: BorderRadius.circular(10),
+      ));
+}
+
+InputDecoration MandalartTextFieldStyle(String labelText) {
+  return InputDecoration(
+    labelText: labelText,
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(10.0), // Set round border
+    ),
+    focusedBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(10.0), // Set round border
+      borderSide: BorderSide(color: Colors.blue), // Set border color
+    ),
+    // Remove underline
+    enabledBorder: OutlineInputBorder(
+      borderSide: BorderSide.none,
+      borderRadius: BorderRadius.circular(10.0),
+    ),
+  );
+}
