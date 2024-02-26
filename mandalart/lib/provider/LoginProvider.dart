96c037c5e8706eacf84caf5710d504e8765e8bd8
isihyeon <tlgus0113@gmail.com>
2024-02-27 00:32:09 +0900

    [feat] Login 관리 Provider 생성

---
+++ mandalart/lib/provider/LoginProvider.dart
@@ -0,0 +1,59 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:mandalart/DataModel/User.dart';
+
+class LoginViewModel extends ChangeNotifier {
+  late final MandalartUser _user = MandalartUser();
+  late final FirebaseAuth _auth = FirebaseAuth.instance;
+  late final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+  Future<void> loginOrLogout() async {
+    if (_user.checkLogin()) {
+      logOut();
+    } else {
+      signInWithGoogle();
+    }
+  }
+
+  String getUserName() {
+    if (_user.name.isEmpty) return throw Exception('No login information');
+    return _user.name;
+  }
+
+  String getUserCollectionName() {
+    if (_user.collection.isEmpty) return throw Exception('No login information');
+    return _user.collection;
+  }
+
+  Future<void> signInWithGoogle() async {
+    try {
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+      final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+
+      final UserCredential userCredential = await _auth.signInWithCredential(credential);
+
+      _user.name = userCredential.user!.displayName!;
+      print('Login with google : ${_user.name}');
+      notifyListeners();
+    } catch (error) {
+      print('error $error');
+    }
+  }
+
+  Future<void> logOut() async {
+    _user.name = '';
+    _user.collection = '';
+    print('Logout : ${_user.name}');
+    notifyListeners();
+  }
+
+  bool checkLogin() {
+    return _user.checkLogin();
+  }
+}
