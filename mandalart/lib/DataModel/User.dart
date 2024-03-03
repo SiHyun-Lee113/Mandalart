c35230f2c41264d844f6e4f610e49bf0ef1b370c
isihyeon <tlgus0113@gmail.com>
2024-03-03 20:13:19 +0900

    [feat] 컬랙션 안의 Document Id 저장용 필드 생성

--- mandalart/lib/DataModel/User.dart
+++ mandalart/lib/DataModel/User.dart
@@ -3,6 +3,7 @@ class MandalartUser {
   String _collectionName = '';
   String _email = '';
   String _photoUrl = '';
+  List<String> _idList = [];
 
   static final MandalartUser _instance = MandalartUser._internal();
   factory MandalartUser() => _instance;
@@ -26,6 +27,11 @@ class MandalartUser {
   String get photoUrl => _photoUrl;
   set photoUrl(String value) {
     _photoUrl = value;
+  }
+
+  List<String> get idList => _idList;
+  set idList(List<String> value) {
+    _idList = value;
   }
 
   bool checkLogin() => _name.isNotEmpty ? true : false;
