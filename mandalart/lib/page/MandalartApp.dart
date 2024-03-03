22fcb0e292a9cba7457bb75d4947b3710af60175
isihyeon <tlgus0113@gmail.com>
2024-03-03 20:09:52 +0900

    [feat] 매인 앱 구동 시 초기화를 위해 FutureBuilder로 변경

--- mandalart/lib/page/MandalartApp.dart
+++ mandalart/lib/page/MandalartApp.dart
@@ -4,6 +4,7 @@ import 'package:mandalart/page/MandalartInputPage.dart';
 import 'package:mandalart/page/MandalartListPage.dart';
 import 'package:mandalart/page/MandalartShowPage.dart';
 import 'package:mandalart/page/MandalartTypeSelectionPage.dart';
+import 'package:mandalart/provider/FirebaseProvider.dart';
 import 'package:provider/provider.dart';
 
 import '../firebase_options.dart';
@@ -17,25 +18,49 @@ Future<void> main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  runApp(MultiProvider(
-      providers: [
-        ChangeNotifierProvider<MandalartProvider>.value(
-            value: MandalartProvider()),
-        ChangeNotifierProvider<LoginViewModel>.value(
-          value: LoginViewModel(),
-        )
-      ],
-      child: MaterialApp(
-        title: '만다라트 계획표 애플리케이션',
-        initialRoute: '/mdList',
-        routes: {
-          '/mdList': (context) => MandalartListPage(),
-          '/mdShow': (context) => MandalartShowPage(),
-          '/mdTypeSelect': (context) => MandalartTypeSelectionPage(),
-          '/mdInputPage': (context) {
-            final args = ModalRoute.of(context)?.settings.arguments as int?;
-            return MandalartInputPage(viewCount: args ?? -1);
+  runApp(
+    MultiProvider(
+        providers: [
+          ChangeNotifierProvider<MandalartProvider>.value(
+              value: MandalartProvider()),
+          ChangeNotifierProvider<UserProvider>.value(
+            value: UserProvider(),
+          ),
+          ChangeNotifierProvider<FirebaseProvider>.value(
+            value: FirebaseProvider(),
+          )
+        ],
+        child: FutureBuilder(
+          future: _initializeApp(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            } else {
+              return MaterialApp(
+                title: '만다라트 계획표 애플리케이션',
+                initialRoute: '/mdList',
+                routes: {
+                  '/mdList': (context) => MandalartListPage(),
+                  '/mdShow': (context) {
+                    var arg =
+                        ModalRoute.of(context)?.settings.arguments as int?;
+                    return MandalartShowPage(docIndex: arg ?? -1);
+                  },
+                  '/mdTypeSelect': (context) => MandalartTypeSelectionPage(),
+                  '/mdInputPage': (context) {
+                    var arg =
+                        ModalRoute.of(context)?.settings.arguments as int?;
+                    return MandalartInputPage(viewCount: arg ?? -1);
+                  },
+                },
+              );
+            }
           },
-        },
-      )));
+        )),
+  );
+}
+
+Future<void> _initializeApp() async {
+  await UserProvider().getUserInfo();
+  await FirebaseProvider().getUserDocsList();
 }
