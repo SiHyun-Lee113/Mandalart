e2c37dfeecda419174ed6a5614979b84bda04cf6
isihyeon <tlgus0113@gmail.com>
2024-03-12 22:34:54 +0900

    [feat] 샘플용 데이터(오타니 만다라트) 출력용 데이터 생성 - 데이터 출력을 위한 Josn To MandalartDto 메서드 추가 : FirebaseProvider2.dart

--- mandalart/lib/provider/FirebaseProvider2.dart
+++ mandalart/lib/provider/FirebaseProvider2.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:mandalart/DataModel/User.dart';
 
 import '../DataModel/Mandalart.dart';
@@ -9,12 +12,37 @@ import '../service/FirebaseService2.dart';
 class FirebaseProvider2 extends ChangeNotifier {
   final MandalartUser _user = MandalartUser();
   final FirebaseService2 fbService = FirebaseService2();
+  final List<MandalartDto> _sampleList = [];
   List<MandalartDto> _mdDtoList = [];
 
   List<MandalartDto> get mdDtoList => _mdDtoList;
 
+  List<MandalartDto> get sampleList => _sampleList;
+
   Stream<QuerySnapshot<Map<String, dynamic>>> getMdDtoListStream() {
     return fbService.getMdDtoStream(_user.collection);
+  }
+
+  Future<List<MandalartDto>> getSampleList() async {
+    String hexagon = await rootBundle
+        .loadString('lib/sampleFiles/OhtaniMandalartHexagon.json');
+    String triangle = await rootBundle
+        .loadString('lib/sampleFiles/OhtaniMandalartTriangle.json');
+    String square = await rootBundle
+        .loadString('lib/sampleFiles/OhtaniMandalartSquare.json');
+
+    Map<String, dynamic> hexMd = jsonDecode(hexagon);
+    Map<String, dynamic> triMd = jsonDecode(triangle);
+    Map<String, dynamic> squMd = jsonDecode(square);
+
+    sampleList.add(MandalartDto('', 8, Mandalart.fromJson(squMd)));
+    sampleList.add(MandalartDto('', 6, Mandalart.fromJson(triMd)));
+    sampleList.add(MandalartDto('', 3, Mandalart.fromJson(hexMd)));
+
+    print('FirebaseProvider2.getSampleList');
+    print(sampleList.toString());
+
+    return sampleList;
   }
 
   Future<void> getMdDtoList() async {
@@ -49,6 +77,6 @@ class FirebaseProvider2 extends ChangeNotifier {
   }
 
   void deleteDocument(MandalartDto mdDto) {
-    fbService.deleteDocument(_user.email, mdDto.docId);
+    fbService.deleteDocument(_user.collection, mdDto.docId);
   }
 }
